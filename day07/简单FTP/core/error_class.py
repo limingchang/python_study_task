a17796ad0d4b2e0a9e2d63d6ca9d35de32c2ec65
limingchang <limich1898@gmail.com>
2017-07-26 17:25:56 +0800

    fix:modify error_class tips message

--- day07/简单FTP/core/error_class.py
+++ day07/简单FTP/core/error_class.py
@@ -11,15 +11,15 @@ class Client_Error(Exception):
 
     def Err_Msg(self,errcode,act):
         if errcode == 0:
-            self.message = '执行[%s]成功'%act
+            self.message = '[客户端]执行[%s]成功'%act
         elif errcode == 2:
-            self.message = '执行[%s]错误:参数错误'%act
+            self.message = '[客户端]执行[%s]错误:参数错误'%act
         elif errcode == 301:
-            self.message = '执行[%s]错误:本地文件不存在' % act
+            self.message = '[客户端]执行[%s]错误:本地文件不存在' % act
         elif errcode == 302:
-            self.message = '执行[%s]错误:本地文件超过最大可上传大小' % act
+            self.message = '[客户端]执行[%s]错误:本地文件超过最大可上传大小' % act
         else:
-            self.message = '执行[%s]错误:未知错误'%act
+            self.message = '[客户端]执行[%s]错误:未知错误'%act
 
     def __str__(self):
         return self.message
@@ -31,27 +31,27 @@ class Server_Error(Exception):
 
     def Err_Msg(self,errcode,act):
         if errcode == 0:
-            self.message = '执行[%s]成功'%act
+            self.message = '[服务端]执行[%s]成功'%act
         elif errcode == -1:
-            self.message = '执行[%s]错误:\033[1;31;1m指令错误\033[0m'%act
+            self.message = '[服务端]执行[%s]错误:\033[1;31;1m指令错误\033[0m'%act
         elif errcode == 1:
-            self.message = '执行[%s]错误:未登录用户不能操作'%act
+            self.message = '[服务端]执行[%s]错误:未登录用户不能操作'%act
         elif errcode == 100:
-            self.message = '执行[%s]成功:登录成功' % act
+            self.message = '[服务端]执行[%s]成功:登录成功' % act
         elif errcode == 101:
-            self.message = '执行[%s]错误:用户名或密码错误' % act
+            self.message = '[服务端]执行[%s]错误:用户名或密码错误' % act
         elif errcode == 102:
-            self.message = '执行[%s]错误:用户不存在' % act
+            self.message = '[服务端]执行[%s]错误:用户不存在' % act
         elif errcode == 2:
-            self.message = '执行[%s]错误:参数错误'%act
+            self.message = '[服务端]执行[%s]错误:参数错误'%act
         elif errcode == 301:
-            self.message = '执行[%s]错误:本地文件不存在' % act
+            self.message = '[服务端]执行[%s]错误:本地文件不存在' % act
         elif errcode == 302:
-            self.message = '执行[%s]错误:文件超过最大可上传大小' % act
+            self.message = '[服务端]执行[%s]错误:文件超过最大可上传大小' % act
         elif errcode == 303:
-            self.message = '执行[%s]错误:云端文件不存在，请检查下载文件名参数' % act
+            self.message = '[服务端]执行[%s]错误:云端文件不存在，请检查下载文件名参数' % act
         else:
-            self.message = '执行[%s]错误:未知错误'%act
+            self.message = '[服务端]执行[%s]错误:未知错误'%act
 
     def __str__(self):
         return self.message

aedbaed3572e40f55c1c1fd63de5aace6465433a
limingchang <limich1898@gmail.com>
2017-08-25 13:47:31 +0800

    <feat>add host action function

--- day09/主机管理/core/host_manage.py
+++ day09/主机管理/core/host_manage.py
@@ -18,10 +18,17 @@ class Host_Manage():
         '''
         self.SSH = SSH_Manage_Obj
         self.DB_Name = self.SSH.DB_NAME
-        #显示菜单
+        self.Main_Menu()
+
+
+    def Main_Menu(self):
+        # 显示菜单
         group_name = self.Show_Menu()
-        #获取主机组下的主机信息字典
+        # 获取主机组下的主机信息字典
         host_dict = self.Get_Host_Dict(group_name)
+        # 显示机组控制菜单
+        self.Show_Host_Menu(group_name, host_dict)
+
 
 
     def Show_Menu(self):
@@ -48,18 +55,19 @@ class Host_Manage():
             return self.Group_List[int(act)]
 
 
-    def Show_Host_Menu(self,host_dict):
+    def Show_Host_Menu(self,group_name,host_dict):
         '''
         主机管理菜单
+        :param group_name: 机组名
         :param host_dict: 主机信息字典
         :return:
         '''
-        menu_list = ['1.执行命令','2.上传/下载','3.新增主机','4.修改主机信息']
+        menu_list = ['0.执行命令','1.上传/下载','2.新增主机','3.修改主机信息']
         menu_dict = {
-            '1.执行命令':'Run_Command',
-            '2.上传/下载':'FTP_File',
-            '3.新增主机':'Add_Host',
-            '4.修改主机信息':'Modify_Host'
+            '0.执行命令':'Run_Command',
+            '1.上传/下载':'FTP_File',
+            '2.新增主机':'Add_Host',
+            '3.修改主机信息':'Modify_Host'
         }
         for menu in menu_list:
             print(menu)
@@ -67,11 +75,114 @@ class Host_Manage():
             act = input('请选择：')
             if act.isdigit() and int(act)<len(menu_list):
                 menu = menu_dict[menu_list[int(act)]]
+                break
             else:
                 print('选择错误！')
                 continue
         func = getattr(self,menu)
-        func(host_dict)
+        func(group_name,host_dict)
+
+    def Run_Command(self,group_name,host_dict):
+        '''
+        执行命令
+        :param host_dict: 主机信息字典
+        :return:
+        '''
+        self.Show_Host_Info(group_name,host_dict)
+        while True:
+            print('在机组执行命令(q退出)'.center(50,'-'))
+            cmd = input('请输入命令(linux):').strip().lower()
+            if cmd == 'q':
+                break
+            else:
+                #print(host_dict)
+                # break
+                tread_list=[]
+                for host in host_dict:
+                    host_tread = threading.Thread(
+                        target=self.SSH.Create_SSHClient,
+                        args=(host_dict[host]['ip'],int(host_dict[host]['port']),host_dict[host]['user'],host_dict[host]['pwd'],cmd)
+                    )
+                    tread_list.append(host_tread)
+                    host_tread.start()
+                for t in tread_list:
+                    t.join()
+                continue
+
+        self.Main_Menu()
+
+    def FTP_File(self,group_name,host_dict):
+        pass
+
+
+    def Add_Host(self,group_name,host_dict):
+        '''
+        在选定的主机组中新增主机
+        :param host_dict: 主机信息字典
+        :return:
+        '''
+        self.Create_Host(group_name)
+        self.Main_Menu()
+
+
+
+    def Modify_Host(self,group_name,host_dict):
+        print('选择要修改的主机'.center(50,'-'))
+        count = 0
+        host_list = []
+        for host in host_dict:
+            host_list.append(host)
+            print(count,'.',host, '(', host_dict[host]['ip'], ')')
+            count += 1
+        while True:
+            act = input('请选择(q退出)：').strip().lower()
+            if act.isdigit() and int(act)< len(host_list):
+                self.Show_Host_Info(group_name,host_dict,host_list[int(act)])
+                ip = input('输入主机地址：')
+                port = input('输入主机端口：')
+                user = input('输入主机用户名：')
+                pwd = input('输入主机密码：')
+                host_dict[host_list[int(act)]] = {
+                    'ip': ip,
+                    'port': port,
+                    'user': user,
+                    'pwd': pwd
+                }
+                # 写入数据
+                host_file = os.path.join(path, self.DB_Name, group_name + '.dat')
+                with open(host_file, 'wb') as f:
+                    pickle.dump(host_dict, f)
+                break
+            elif act == 'q':
+                break
+            else:
+                print('选择错误！')
+        self.Main_Menu()
+
+
+    def Show_Host_Info(self,group_name,host_dict,mode=''):
+        '''
+        显示主机信息
+        :param group_name: 机组名称
+        :param host_dict: 主机信息字典
+        :param mode: 空为简显示，需要单个主机完整信息时mode为主机名称（用于修改主机信息）
+        :return:
+        '''
+        if mode =='':
+            print('主机组【%s】包含以下主机'.center(50, '-') % (group_name))
+            for host in host_dict:
+                print(host, '(', host_dict[host]['ip'], ')')
+        else:
+            if mode in host_dict:
+                print('主机【%s】信息如下：'.center(50,'-') %mode)
+                print('主机地址:',host_dict[mode]['ip'])
+                print('SSH端口:', host_dict[mode]['port'])
+                print('用户名:', host_dict[mode]['user'])
+                print('密码:', host_dict[mode]['pwd'])
+                print('-'*50)
+            else:
+                print('主机名称错误！')
+
 
 
     def Get_Host_Group(self):

2823f224da9aa80a710590aeb004f33c16c6fa7e
limingchang <limich1898@gmail.com>
2017-09-07 21:24:47 +0800

    <add>add send_cmd func

--- day10/rpc主机管理/RPC_Client/core/client_class.py
+++ day10/rpc主机管理/RPC_Client/core/client_class.py
@@ -5,7 +5,7 @@ import sys, os
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, path)
 
-import pika,configparser,random
+import pika,configparser,random,pickle
 
 class RPC_Client(object):
     '''
@@ -24,6 +24,9 @@ class RPC_Client(object):
         # print(self.__dict__)
         # 创建结果空字典，保存命令结果
         self.Res_Dict = {}
+        #创建任务ID空列表
+        self.Task_List = []
+
 
     def Get_Conf(self):
         config = configparser.ConfigParser()
@@ -59,14 +62,19 @@ class RPC_Client(object):
                 if len(act_list) == 3:
                     cmd = act_list[1]
                     host_str = act_list[2].strip()
-                    host_grop = host_str.split(' ')
-                    host_list = []
-                    for i in range(1,len(host_grop)):
-                        host_list.append(host_grop[i])
-                    self.Run(cmd,host_list)
+                    if '--host' not in host_str:
+                        print('Parameter Error!')
+                    else:
+                        host_grop = host_str.split(' ')
+                        host_list = []
+                        for i in range(1,len(host_grop)):
+                            host_list.append(host_grop[i])
+                        #这里创建多线程
+                        self.Run(cmd,host_list)
                 else:
                     print('\033[1;31;1mCommand Error!\033[0m')
                     self.Help()
+                continue
             elif act.startswith('check_task'):
                 if len(act_list) == 2:
                     task_id = act_list[1]
@@ -74,8 +82,9 @@ class RPC_Client(object):
                 else:
                     print('\033[1;31;1mCommand Error!\033[0m')
                     self.Help()
+                continue
             elif act == 'quit':
-                exit()
+                break
             else:
                 print('\033[1;31;1mCommand Error!\033[0m')
                 self.Help()
@@ -101,6 +110,63 @@ class RPC_Client(object):
         '''
         print(cmd)
         print(host_list)
+        self.Handler()
+        result = self.Channel.queue_declare(exclusive=True)
+        self.Response_Queue = result.method.queue#创建随机queue
+        #创建接收端消费者，用于接收返回结果
+        self.Channel.basic_consume(
+            self.On_Response,#callback
+            no_ack=True,
+            queue=self.Response_Queue
+        )
+
+
+    def Send_Cmd(self,cmd,host_list):
+        task_id = self.Create_TaskID()
+        #self.Res_Dict[task_id] = None
+        #将返回结果初始化为空
+        for host in host_list:
+            self.Res_Dict[task_id][host] = None
+        #构建消息数据
+        data = {
+            'cmd':cmd,
+            'host_list':host_list
+        }
+        #加入任务ID列表
+        self.Task_List.append(task_id)
+        #self.corr_id = str(task_id)
+        #创建发送端
+        self.Channel.basic_publish(
+            exchange='',
+            routing_key='rpc',
+            properties=pika.BasicProperties(
+                #标识返回queue
+                reply_to=self.Response_Queue,
+                #标识命令任务ID
+                correlation_id=str(task_id)
+            ),
+            body=pickle.dumps(data)
+        )
+        #循环接收结果
+        for host in host_list:
+            while self.Res_Dict[task_id][host] is None:
+                self.Conn.process_data_events()
+            print('收到主机【%s】返回消息'%host)
+        print('命令【%s】=>'%cmd,host_list,'执行完毕！')
+        print('请使用check_task命令获取结果')
+
+
+    def On_Response(self,ch,method,props,body):
+        '''
+        处理返回结果的函数
+        :return:
+        '''
+        task_id = props.correlation_id
+        res_data = pickle.loads(body)
+        #返回为字典{host:res}
+        self.Res_Dict[task_id][res_data['host']] = res_data['res']
+
+
 
     def Create_TaskID(self):
         '''
@@ -115,7 +181,7 @@ class RPC_Client(object):
         if task_id in self.Res_Dict:
             self.Create_TaskID()
         else:
-            self.Res_Dict[task_id]=''
+            self.Res_Dict[task_id] = None
         return task_id
 
     def Get_Result(self,id):
@@ -130,6 +196,6 @@ class RPC_Client(object):
 
 if __name__ == "__main__":
     rpc_cli = RPC_Client()
-    #rpc_cli.Command_Parse()
-    a = rpc_cli.Create_TaskID()
-    print(a)
+    rpc_cli.Command_Parse()
+    #a = rpc_cli.Create_TaskID()
+    #print(a)

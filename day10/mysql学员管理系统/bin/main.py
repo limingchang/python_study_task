e626034f7a911562ff93cb75ee25e91421bbda6f
limingchang <limich1898@gmail.com>
2017-09-20 15:46:13 +0800

    <add>add default data write func

---
+++ day10/mysql学员管理系统/bin/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Mc.Lee'
+import sys, os
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, path)
+
+from core.view_class import View_Interface
+
+View = View_Interface()
